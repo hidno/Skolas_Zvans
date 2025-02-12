0462331873f39488613212786af0ddc8a778c5d6
Lauris_Z <lauriszuselis9@gmail.com>
2025-02-13 00:58:44 +0200

    Izlaboju failu pārejas funkcionalitāti un direktoriju

--- Proj/Iestatījumi.py
+++ Proj/Iestatījumi.py
@@ -1,4 +1,4 @@
-import subprocess
+import os, subprocess
 import tkinter as tk
 import tkinter.font as tkFont
 
@@ -14,8 +14,11 @@ Mina = tkFont.Font(family="Mina", size=20)
 canvas = tk.Canvas(logs, bg="#E7E2E2")
 canvas.pack(fill=tk.BOTH, expand=True)
 
-def back():
-    subprocess.Popen(['python','Skolas_Zvans.py'])
+os.chdir(r"C:\Users\Hidno\Documents\Prog\Skolas_Zvans\Proj")
+
+def back(event):
+    subprocess.Popen(['python', 'Skolas_Zvans.py'])
+    logs.destroy()
  
 atpakaļ_koordinātes = [(0, loga_augstums * 0.725),(loga_platums * 0.35, loga_augstums * 0.725),(loga_platums * 0.39, loga_augstums * 0.8),(loga_platums * 0.35, loga_augstums * 0.875),(0, loga_augstums * 0.875)]
 atpakaļ = canvas.create_polygon(0, 575, 450, 575, 500, 640, 450, 700, 0, 700, fill="#7A2222")
--- Proj/Skolas_Zvans.py
+++ Proj/Skolas_Zvans.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from PIL import Image, ImageTk
-import subprocess
 import tkinter.font as tkFont
+import os, subprocess
 
 logs = tk.Tk()
 logs.title("Skolas Zvans")
@@ -12,17 +12,17 @@ logs.configure(bg="#E7E2E2")
 
 canvas = tk.Canvas(logs, bg="#E7E2E2")
 canvas.pack(fill=tk.BOTH, expand=True)
+os.chdir(r"C:\Users\Hidno\Documents\Prog\Skolas_Zvans\Proj")
 
 def lādē(file):
     img = Image.open(file)
     img = img.convert('RGBA')  
     return img
 
-pulksteņa_att = lādē(r"C:\Users\Hidno\Documents\Prog\Skolas_Zvans\Proj\clock_p.png")
+pulksteņa_att = lādē("clock_p.png")
 pulksteņa_att = pulksteņa_att.resize((400, 400))
 
 pulksteņa_att_tk = ImageTk.PhotoImage(pulksteņa_att)
-
 
 foto1_lokācija = tk.Label(logs, image=pulksteņa_att_tk, bg="#E7E2E2")
 foto1_lokācija.image = pulksteņa_att_tk 
@@ -38,8 +38,9 @@ sākt = canvas.create_polygon(sākt_koordinātes, fill="#7A2222")
 canvas.create_text(loga_platums * 0.225, loga_augstums * 0.6, text="Sākt Spēli", font=(Mina, 90), fill="white")
 canvas.tag_bind(sākt, '<Button-1>', start_game)
 
-def settings():
-    subprocess.Popen(['python','Iestatījumi.py'])
+def settings(event):
+    subprocess.Popen(['python', 'Iestatījumi.py'])
+    logs.destroy()
 
 iestatīt_koordinātes = [(0, loga_augstums * 0.725),(loga_platums * 0.35, loga_augstums * 0.725),(loga_platums * 0.39, loga_augstums * 0.8),(loga_platums * 0.35, loga_augstums * 0.875),(0, loga_augstums * 0.875)]
 iestatīt = canvas.create_polygon(iestatīt_koordinātes, fill="#4A0A0A")
