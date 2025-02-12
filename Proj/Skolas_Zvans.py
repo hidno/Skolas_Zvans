20a303cf29b7be558256b63a145ad72e53a7969f
Lauris_Z <lauriszuselis9@gmail.com>
2025-02-12 22:34:00 +0200

    Automatizēju pogu novietošanas funkciju

--- Proj/Skolas_Zvans.py
+++ Proj/Skolas_Zvans.py
@@ -32,17 +32,19 @@ Mina = tkFont.Font(family="Mina", size=20)
 
 def start_game():
     print("Sākt Spēli")
-     
-sākt = canvas.create_polygon(0, 400, 550, 400, 600, 475, 550, 550, 0, 550, fill="#7A2222")
-canvas.create_text(270, 475, text="Sākt Spēli", font=(Mina,50), fill="white", )
+
+sākt_koordinātes = [(0, loga_augstums * 0.5),(loga_platums * 0.45, loga_augstums * 0.5),(loga_platums * 0.49, loga_augstums * 0.6),(loga_platums * 0.45, loga_augstums * 0.7),(0, loga_augstums * 0.7)]
+sākt = canvas.create_polygon(sākt_koordinātes, fill="#7A2222")
+canvas.create_text(loga_platums * 0.225, loga_augstums * 0.6, text="Sākt Spēli", font=(Mina, 90), fill="white")
 canvas.tag_bind(sākt, '<Button-2>', start_game)
 
 def settings():
     logs.destroy()
     subprocess.run(["python", "Iestatījumi.py"])
 
-iestatīt = canvas.create_polygon(0, 575, 450, 575, 500, 640, 450, 700, 0, 700, fill="#4A0A0A")
-canvas.create_text(225, 637, text="Iestatījumi", font=(Mina,40), fill="white")
+iestatīt_koordinātes = [(0, loga_augstums * 0.725),(loga_platums * 0.35, loga_augstums * 0.725),(loga_platums * 0.39, loga_augstums * 0.8),(loga_platums * 0.35, loga_augstums * 0.875),(0, loga_augstums * 0.875)]
+iestatīt = canvas.create_polygon(iestatīt_koordinātes, fill="#4A0A0A")
+canvas.create_text(loga_platums * 0.175, loga_augstums * 0.8, text="Iestatījumi", font=(Mina, 50), fill="white")
 canvas.tag_bind(iestatīt, '<Button-1>', settings)
 
 logs.mainloop()
