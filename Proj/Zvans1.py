1321681b396abc6edfc180bed985c761ef044d61
Lauris_Z <lauriszuselis9@gmail.com>
2025-02-13 20:33:18 +0200

    Uzlabota kopējā koda struktūra

--- Proj/Zvans1.py
+++ Proj/Zvans1.py
@@ -9,12 +9,12 @@ logs.state('zoomed')
 loga_augstums = logs.winfo_screenheight()
 loga_platums = logs.winfo_screenwidth()
 logs.configure(bg="#87ceeb")
-
 canvas = tk.Canvas(logs, bg="#87ceeb")
 canvas.pack(fill=tk.BOTH, expand=True)
 os.chdir(r"C:\Users\Hidno\Documents\Prog\Skolas_Zvans\Proj")
-
 Mina = tkFont.Font(family="Mina", size=20)
+
+
 
 def lādē(file):
     img = Image.open(file)
@@ -24,29 +24,32 @@ def lādē(file):
 with open('dati.json', 'r') as file:
     data = json.load(file)
 
+dienas_sākums = data.get("dienas_sakums", "")
+
 def next(event):
     subprocess.Popen(['python', 'Zvans2.py'])
     logs.withdraw()
 
-kvadrāts_koordinātes = [(0, loga_augstums*0.6), (loga_platums, loga_augstums*0.6), (loga_platums, loga_augstums), (0, loga_augstums)]
-kvadrāts = canvas.create_polygon(kvadrāts_koordinātes, fill="#345f3b")
+
+
+Kvadrāts_koordinātes = [(0, loga_augstums*0.6), (loga_platums, loga_augstums*0.6), (loga_platums, loga_augstums), (0, loga_augstums)]
+Kvadrāts = canvas.create_polygon(Kvadrāts_koordinātes, fill="#345f3b")
 
 skola_att = lādē("skool.png")
 skola_att = skola_att.resize((960, 520))
 skola_att_tk = ImageTk.PhotoImage(skola_att)
 canvas.create_image((loga_platums-960)//2, loga_augstums*0.63 -520, image=skola_att_tk, anchor=tk.NW)
 
-kvadrāts1_koordinātes = [(loga_platums*0.46, loga_augstums*0.63), (loga_platums*0.53, loga_augstums*0.63), (loga_platums*0.53, loga_augstums), (loga_platums*0.46, loga_augstums)]
-kvadrāts1 = canvas.create_polygon(kvadrāts1_koordinātes, fill="#e1ddbf", width=2)
+Kvadrāts1_koordinātes = [(loga_platums*0.46, loga_augstums*0.63), (loga_platums*0.53, loga_augstums*0.63), (loga_platums*0.53, loga_augstums), (loga_platums*0.46, loga_augstums)]
+Kvadrāts1 = canvas.create_polygon(Kvadrāts1_koordinātes, fill="#e1ddbf", width=2)
 
-kvadrāts2_koordinātes = [(0, 0), (loga_platums * 0.2, 0), (loga_platums * 0.2, loga_augstums * 0.15), (0, loga_augstums * 0.15)]
-kvadrāts2 = canvas.create_polygon(kvadrāts2_koordinātes, fill="#8ea2b1")
-dienas_sākums = data.get("dienas_sakums", "")
+Kvadrāts2_koordinātes = [(0, 0), (loga_platums * 0.2, 0), (loga_platums * 0.2, loga_augstums * 0.15), (0, loga_augstums * 0.15)]
+Kvadrāts2 = canvas.create_polygon(Kvadrāts2_koordinātes, fill="#8ea2b1")
 canvas.create_text(loga_platums * 0.1, loga_augstums * 0.075, text=dienas_sākums, font=(Mina, 50), fill="white")
 
 Turpināt_koordinātes = [(loga_platums * 0.75, loga_augstums*0.725), (loga_platums* 0.95, loga_augstums*0.725), (loga_platums*0.95,loga_augstums* 0.875), (loga_platums *0.75, loga_augstums* 0.875)]
-turpināt_poga = canvas.create_polygon(Turpināt_koordinātes, fill="#7A2222")
+Turpināt_poga = canvas.create_polygon(Turpināt_koordinātes, fill="#7A2222")
 canvas.create_text(loga_platums * 0.85, loga_augstums * 0.8, text="Turpināt", font=(Mina, 50), fill="white")
-canvas.tag_bind(turpināt_poga, '<Button-1>', next)
+canvas.tag_bind(Turpināt_poga, '<Button-1>', next)
 
 logs.mainloop()
--- Proj/Zvans4.py
+++ Proj/Zvans4.py
@@ -10,12 +10,12 @@ logs.state('zoomed')
 loga_augstums = logs.winfo_screenheight()
 loga_platums = logs.winfo_screenwidth()
 logs.configure(bg="#4682b4")
-
 canvas = tk.Canvas(logs, bg="#4682b4")
 canvas.pack(fill=tk.BOTH, expand=True)
 os.chdir(r"C:\Users\Hidno\Documents\Prog\Skolas_Zvans\Proj")
-
 Mina = tkFont.Font(family="Mina", size=20)
+
+
 
 def lādē(file):
     img = Image.open(file)
@@ -28,6 +28,7 @@ with open('dati.json', 'r') as file:
 with open('dienas_laiks.txt', 'r') as file:
     laiks = file.read()
 
+dienas_beigas = data.get("dienas_beigas", "")
 stundu_intervāls = data.get("stundu_intervals", "")
 laika_formāts = "%H:%M"  
 
@@ -38,38 +39,43 @@ def formula():
     with open('dienas_laiks.txt', 'w') as file:
         file.write(rēķins.strftime(laika_formāts))
 
-kvadrāts_koordinātes = [(0, loga_augstums*0.6), (loga_platums, loga_augstums*0.6), (loga_platums, loga_augstums), (0, loga_augstums)]
-kvadrāts = canvas.create_polygon(kvadrāts_koordinātes, fill="#345f3b")
+formula()
+with open('dienas_laiks.txt', 'r') as file:
+    laiks = file.read()
+Kvadrāts2_koordinātes = [(0, 0), (loga_platums * 0.2, 0), (loga_platums * 0.2, loga_augstums * 0.15), (0, loga_augstums * 0.15)]
+Kvadrāts2 = canvas.create_polygon(Kvadrāts2_koordinātes, fill="#6d7c95")
+canvas.create_text(loga_platums * 0.1, loga_augstums * 0.075, text=laiks, font=(Mina, 50), fill="white")
+
+def next(event):
+    laiks_tagad = datetime.strptime(laiks, laika_formāts)
+    dienas_beigas_laiks = datetime.strptime(dienas_beigas, laika_formāts)
+    maiņa = datetime.strptime("18:30", "%H:%M")
+    if laiks_tagad >= dienas_beigas_laiks:
+        subprocess.Popen(['python', 'Skolas_Zvans.py'])
+        logs.withdraw()
+    elif laiks_tagad < maiņa:
+        subprocess.Popen(['python', 'Zvans4.py'])
+        logs.withdraw()
+    else:
+        subprocess.Popen(['python', 'Zvans5.py'])
+        logs.withdraw()
+
+
+
+Kvadrāts_koordinātes = [(0, loga_augstums*0.6), (loga_platums, loga_augstums*0.6), (loga_platums, loga_augstums), (0, loga_augstums)]
+Kvadrāts = canvas.create_polygon(Kvadrāts_koordinātes, fill="#345f3b")
 
 skola_att = lādē("skool.png")
 skola_att = skola_att.resize((960, 520))
 skola_att_tk = ImageTk.PhotoImage(skola_att)
 canvas.create_image((loga_platums-960)//2, loga_augstums*0.63 -520, image=skola_att_tk, anchor=tk.NW)
 
-kvadrāts1_koordinātes = [(loga_platums*0.46, loga_augstums*0.63), (loga_platums*0.53, loga_augstums*0.63), (loga_platums*0.53, loga_augstums), (loga_platums*0.46, loga_augstums)]
-kvadrāts1 = canvas.create_polygon(kvadrāts1_koordinātes, fill="#e1ddbf", width=2)
-
-formula()
-with open('dienas_laiks.txt', 'r') as file:
-    laiks = file.read()
-kvadrāts2_koordinātes = [(0, 0), (loga_platums * 0.2, 0), (loga_platums * 0.2, loga_augstums * 0.15), (0, loga_augstums * 0.15)]
-kvadrāts2 = canvas.create_polygon(kvadrāts2_koordinātes, fill="#6d7c95")
-canvas.create_text(loga_platums * 0.1, loga_augstums * 0.075, text=laiks, font=(Mina, 50), fill="white")
-
-dienas_beigas = data.get("dienas_beigas", "")
-def next(event):
-    laiks_tagad = datetime.strptime(laiks, laika_formāts)
-    dienas_beigas_laiks = datetime.strptime(dienas_beigas, laika_formāts)
-    if laiks_tagad >= dienas_beigas_laiks:
-        subprocess.Popen(['python', 'Skolas_Zvans.py'])
-        logs.withdraw()
-    else:
-        subprocess.Popen(['python', 'Zvans5.py'])
-        logs.withdraw()
+Kvadrāts1_koordinātes = [(loga_platums*0.46, loga_augstums*0.63), (loga_platums*0.53, loga_augstums*0.63), (loga_platums*0.53, loga_augstums), (loga_platums*0.46, loga_augstums)]
+Kvadrāts1 = canvas.create_polygon(Kvadrāts1_koordinātes, fill="#e1ddbf", width=2)
 
 Turpināt_koordinātes = [(loga_platums * 0.75, loga_augstums*0.725), (loga_platums* 0.95, loga_augstums*0.725), (loga_platums*0.95,loga_augstums* 0.875), (loga_platums *0.75, loga_augstums* 0.875)]
-turpināt_poga = canvas.create_polygon(Turpināt_koordinātes, fill="#7A2222")
+Turpināt_poga = canvas.create_polygon(Turpināt_koordinātes, fill="#7A2222")
 canvas.create_text(loga_platums * 0.85, loga_augstums * 0.8, text="Turpināt", font=(Mina, 50), fill="white")
-canvas.tag_bind(turpināt_poga, '<Button-1>', next)
+canvas.tag_bind(Turpināt_poga, '<Button-1>', next)
 
 logs.mainloop()
