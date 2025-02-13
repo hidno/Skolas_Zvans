cbc4b42903a9bdc339ed66832de44c07e42db204
Lauris_Z <lauriszuselis9@gmail.com>
2025-02-13 05:45:42 +0200

    Pielabots attēla kods

--- Proj/Zvans1.py
+++ Proj/Zvans1.py
@@ -21,18 +21,18 @@ def lādē(file):
     img = img.convert('RGBA')  
     return img
 
+def next(event):
+    print("Turpināt")
+
 skola_att = lādē("skool.png")
 skola_att = skola_att.resize((640, 360))
 skola_att_tk = ImageTk.PhotoImage(skola_att)
-skola_lokācija = tk.Label(logs, image=skola_att_tk, bg="#87ceeb")
+skola_lokācija = tk.Label(canvas, image=skola_att_tk, bg="#87ceeb")
 skola_lokācija.image = skola_att_tk 
-skola_lokācija.place(x=((loga_platums-640)//2), y=loga_augstums*0.63 -360)
+skola_lokācija.place(x=((loga_platums-640)//2), y=loga_augstums*0.6 -360)
 
 kvadrāts_koordinātes = [(0, loga_augstums*0.6), (loga_platums, loga_augstums*0.6), (loga_platums, loga_augstums), (0, loga_augstums)]
 kvadrāts = canvas.create_polygon(kvadrāts_koordinātes, fill="#345f3b")
-
-def next(event):
-    print("Turpināt")
 
 Turpināt_koordinātes = [(loga_platums * 0.75, loga_augstums*0.725), (loga_platums* 0.95, loga_augstums*0.725), (loga_platums*0.95,loga_augstums* 0.875), (loga_platums *0.75, loga_augstums* 0.875)]
 turpināt_poga = canvas.create_polygon(Turpināt_koordinātes, fill="#7A2222")
