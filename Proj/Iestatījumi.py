1343c4832f27cfd4e5c70bc2385a4e841915ea4a
[Lauris Zuselis] <[lauriszuselis9@gmail.com]>
2025-02-17 10:00:05 +0200

    Izveidoti parmetri, lai automātiski nolasītu mapes direktoriju

--- Proj/Iestatījumi.py
+++ Proj/Iestatījumi.py
@@ -17,7 +17,8 @@ loga_platums = logs.winfo_screenwidth()
 canvas = tk.Canvas(logs, bg="#E7E2E2")
 canvas.pack(fill=tk.BOTH, expand=True)
 
-os.chdir(r"C:\Users\Hidno\Documents\Prog\Skolas_Zvans\Proj")
+projekta_ceļš = os.path.join(os.path.expanduser("~"), "Documents","Proj","Skolas_Zvans","Proj")
+os.chdir(projekta_ceļš)
 Mina = tkFont.Font(family="Mina", size=20)
 
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Skolas_Zvans
 Sistēmas Projeckts - Skolas Zvans
+Nosacījumi Failu Darbībai:
+1. Sagabā Mapi "Dokumenti" mapē
+2. Pip install Pillow
+3. Pip install pygame
